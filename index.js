e5a45beb596afb906c0f2ea8c0e94fdc357cb9ad
agnynureza <rezaagny@gmail.com>
2020-08-30 04:42:16 +0700

    Documentation: add Procfile for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8010;
+const port = process.env.PORT || 8010;
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
